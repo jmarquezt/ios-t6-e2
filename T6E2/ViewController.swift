bdabab59b4887526e1e7de1e6a842e9f6dc20f2d
Jorge Marquez Torres <jmarquez@jorges-mac.local>
2016-02-08 18:29:19 +0100

    ejercicio 2 tema 5

--- T6E2/ViewController.swift
+++ T6E2/ViewController.swift
@@ -9,8 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var filePath: NSString = ""
+    var fileNameAndPath = NSURL()
+    var image = CIImage()
 
     override func viewDidLoad() {
+        image = CIImage(image: UIImage(named: "caras")!)!
+        let context = CIContext(options: nil)
+        let options = NSDictionary(object: CIDetectorAccuracyHigh, forKey: CIDetectorAccuracy)
+        let detector = CIDetector(ofType: CIDetectorTypeFace, context: context, options: options as? [String : AnyObject])
+        let features: NSArray = detector.featuresInImage(image, options: [CIDetectorSmile:true,CIDetectorEyeBlink:true])
+        let imageView = UIImageView(image: UIImage(named: "caras")!)
+        self.view.addSubview(imageView)
+        //Vista auxiliar que luego invertiremos.
+        let vistAux = UIView(frame: imageView.frame)
+        for faceFeature in features {
+            //Detección
+            //let smile = faceFeature.hasSmile
+            //var rightEyeBlinking = faceFeature.rightEyeClosed
+            //var leftEyeBlinking = faceFeature.leftEyeClosed
+            //Localizar la cara
+            let faceRect = faceFeature.bounds
+            let faceView = UIView(frame: faceRect)
+            faceView.layer.borderWidth = 2
+            faceView.layer.borderColor = UIColor.redColor().CGColor
+            //var faceWidth:CGFloat = faceRect.size.width
+            vistAux.addSubview(faceView)
+        }
+        self.view.addSubview(vistAux)
+        //Invertimos el Eje de Coordenadas
+        vistAux.transform = CGAffineTransformMakeScale(1, -1)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
